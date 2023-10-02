b881096090a86bb75b71ef499c42bd9b908a0907
BorseHaraldsen <Augustborse@gmail.com>
2023-10-02 14:06:34 +0200

    Remove comment on filter in main. Add correct names

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import jakarta.servlet.DispatcherType;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -17,7 +19,7 @@ public class Main {
             var webApp = new WebAppContext(resource, "/");
 
             webApp.addServlet(new ServletHolder(new ApiServlet()), "/api/parts"); //Anyone on localhost:8181/api it will go to apiservlet.
-            // webApp.addFilter(new FilterHolder(new NewFilter()), "/*", EnumSet.of(DispatcherType.REQUEST));
+            webApp.addFilter(new FilterHolder(new ApiFilter()), "/*", EnumSet.of(DispatcherType.REQUEST));
             server.setHandler(webApp);
             server.start();
 
