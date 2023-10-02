76aff44895647161cefba942a119253283006853
BorseHaraldsen <Augustborse@gmail.com>
2023-10-02 13:44:30 +0200

    Continuing the get and post requests

--- src/main/java/org/example/ApiServlet.java
+++ src/main/java/org/example/ApiServlet.java
@@ -6,12 +6,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 public class ApiServlet extends HttpServlet {
 
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // /api/parts?name=
+        // make it a getall for simplicity
+        Part.getParts().forEach(p -> {
+            try {
+                resp.getWriter().write(p);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }); // WHEN WE WANT TO WRITE SOMETHING TO THE BODY WE USE GETWRITER
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Gonna take in some sort of body
+        var newPart = req.getReader().lines().collect(Collectors.joining());
+        Part. addParts(newPart);
+        // resp.setStatus(200); // Maybe? Does it automatically potentially.
+
 
     }
 }
--- src/main/java/org/example/Part.java
+++ src/main/java/org/example/Part.java
@@ -6,5 +6,12 @@ import java.util.List;
 public class Part {
 
     public static List<String> parts = List.of("Gear", "cog", "belt");
+    public static List<String> getParts() {
+        return parts;
+    }
+
+    public static void addParts (String parts) {
+        return;
+    }
 
 }
